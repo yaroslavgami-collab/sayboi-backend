ba1ee988ba4543b78b6931b427cf30424e863647
yaroslavgami-collab <yaroslavgami@gmail.com>
2026-08-02 23:11:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-import os
 
 PUBLIC_KEY = os.getenv("LIQPAY_PUBLIC_KEY")
 PRIVATE_KEY = os.getenv("LIQPAY_PRIVATE_KEY")
 
 BASE_URL = "https://sayboi-backend.onrender.com"
+
+print("PUBLIC KEY:", bool(PUBLIC_KEY))
+print("PRIVATE KEY:", bool(PRIVATE_KEY))
+print("BASE URL:", BASE_URL)
