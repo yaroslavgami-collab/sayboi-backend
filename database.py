e3d9984f9f66c68f69824b00e65fc96017518098
yaroslavgami-collab <yaroslavgami@gmail.com>
2026-08-02 20:08:55 +0300

    Update database.py

--- database.py
+++ database.py
@@ -108,9 +108,15 @@ def create_purchase(telegram_id, course, amount, order_id):
 
     conn.execute("""
         INSERT INTO purchases
-        (telegram_id, course, amount, order_id, status)
+               (telegram_id, course, amount, order_id, status)
         VALUES (?, ?, ?, ?, ?)
     """, (
+        telegram_id,
+        course,
+        amount,
+        order_id,
+        "pending"
+    ))
         telegram_id,
         course,
         amount,
