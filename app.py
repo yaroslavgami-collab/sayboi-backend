00fe67b69b85ac497e814de3962b8a9741dd9358
yaroslavgami-collab <yaroslavgami@gmail.com>
2026-08-02 19:38:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, request, jsonify
 from database import init_db
 from flask_cors import CORS
 
+
 init_db()
 from database import (
-    init_db,
-    create_purchase
+    init_database,
+    create_purchase,
+    get_purchase,
+    complete_purchase,
+    activate_premium
 )
 
 from liqpay_service import liqpay
@@ -102,8 +106,40 @@ def create_payment():
     })
 
 import os
+import base64
+import json
 
+
+@app.route("/callback", methods=["POST"])
+def liqpay_callback():
+
+    data = request.form.get("data")
+
+    if not data:
+        return "No data", 400
+
+    decoded = json.loads(
+        base64.b64decode(data).decode("utf-8")
+    )
+
+    status = decoded.get("status")
+    order_id = decoded.get("order_id")
+
+    if status != "success":
+        return "Ignored", 200
+
+    purchase = get_purchase(order_id)
+
+    if purchase is None:
+        return "Purchase not found", 404
+
+    telegram_id = purchase["telegram_id"]
+
+    activate_premium(telegram_id)
+
+    complete_purchase(order_id)
+
+    return "OK", 200
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)\
-
+    app.run(host="0.0.0.0", port=port)
