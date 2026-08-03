4d22482d5a8489257f688cc91f8a54e4e5c102be
yaroslavgami-collab <yaroslavgami@gmail.com>
2026-08-03 22:03:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,27 @@
 import os
 
+
+# =========================
+# LiqPay
+# =========================
+
 PUBLIC_KEY = os.getenv("LIQPAY_PUBLIC_KEY")
 PRIVATE_KEY = os.getenv("LIQPAY_PRIVATE_KEY")
 
+
+# =========================
+# Backend
+# =========================
+
 BASE_URL = "https://sayboi-backend.onrender.com"
 
-print("PUBLIC KEY:", bool(PUBLIC_KEY))
-print("PRIVATE KEY:", bool(PRIVATE_KEY))
-print("BASE URL:", BASE_URL)
+
+# =========================
+# Проверка настроек
+# =========================
+
+if not PUBLIC_KEY:
+    raise RuntimeError("LIQPAY_PUBLIC_KEY is not set")
+
+if not PRIVATE_KEY:
+    raise RuntimeError("LIQPAY_PRIVATE_KEY is not set")
